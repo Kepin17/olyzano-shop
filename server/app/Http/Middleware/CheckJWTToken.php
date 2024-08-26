4bb5d9fe79ffa38e90aab00110af33f595b68c57
Roihan-Suka-Coding <roihanarrafli@gmail.com>
2024-08-26 11:28:23 +0700

    update middleware auth with jwt

---
+++ server/app/Http/Middleware/CheckJWTToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckJWTToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('Processing route: ' . $request->path());
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['message' => 'Token is Invalid'], Response::HTTP_UNAUTHORIZED);
+            } elseif ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['message' => 'Token is Expired'], Response::HTTP_UNAUTHORIZED);
+            } else {
+                return response()->json(['message' => 'Authorization Token not found'], Response::HTTP_UNAUTHORIZED);
+            }
+        }
+        return $next($request);
+    }
+}
